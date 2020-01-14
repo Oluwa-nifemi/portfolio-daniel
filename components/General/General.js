d3ab43867a015248562240b5f802e7c6d59b66fa
Nifemi <deyeminifemi@gmail.com>
2020-01-14 02:55:50 +0100

    Created General Layout Component for global styles and fonts

---
+++ components/General/General.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './general.css'
+
+const General = ({children}) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default General;
